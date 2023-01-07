c48a9a3a9706e910203e29d5104d5b6918dc1718
Mohamed Khaled <mokhaled.work@gmail.com>
2023-01-07 22:01:43 +0200

    Fixed link mixed content

--- src/apis/locations.js
+++ src/apis/locations.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchLocations = async (location) => {
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=422965f1bdf7e50a313aaa23c009d89a`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=422965f1bdf7e50a313aaa23c009d89a`
     );
     return response.data;
   } catch (error) {
